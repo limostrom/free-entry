49c061b33ee191495f212c4f5ee514045d573d2b
Lauren Mostrom <52218040+limostrom@users.noreply.github.com>
2023-07-10 15:45:35 -0500

    pull age 0 firm tabulation from BDS API

--- bds_api_pull.r
+++ bds_api_pull.r
@@ -5,17 +5,39 @@ library(jsonlite)
 api_key <- "a9849ca3e1f42d9273a95763547efb5db955c261"
 base_url <- "https://api.census.gov/data/timeseries/bds"
 
-outfolder <-"/Users/laurenmostrom/Library/CloudStorage/Dropbox/Personal Document Backup/Booth/Second Year/Y2 Paper/Finance & Dynamism/processed-data/bds/"
+outfolder <-"/Users/laurenmostrom/Library/CloudStorage/Dropbox/Personal Document Backup/Booth/Second Year/Y2 Paper/Finance & Dynamism/raw-data/bds/"
 
 # Define the desired variables and filters
-variables <- c("NAME", "NAICS_LABEL", "YEAR", "FIRM", "ESTAB", "EMP")
+variables <- c("NAME", "NAICS_LABEL", "YEAR", "FAGE", "FIRM", "ESTAB", "EMP")
 
 years <- c(1997:2020)
 # Note: dropping public administration (92) and unclassified (99) from the NAICS codes
 inds <- c("00", 11, 21, 22, 23, "31-33", 42, "44-45", "48-49", 51, 52, 53, 54, 55, 56, 61, 62, 71, 72, 81)
 
-    for (i in inds) {
-for (y in years) {
+# Pull firms age 0 by year, NAICS 2-digit sector, and MSA
+for (i in inds) {
+    for (y in years) {
+        # Construct the full URL with parameters
+        url <- paste0(base_url, "?get=", paste(variables, collapse = ","), "&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&time=", y, "&NAICS=", i, "&FAGE=010", "&key=", api_key)
+
+        # Send the HTTP GET request and parse the response
+        response <- GET(url)
+        d <- fromJSON(content(response, "text"))
+        
+        # Remove the first row (header)
+        d <- d[-1, ]
+        d <- as.data.frame(d)
+
+        # Write to CSV
+        write.csv(d, paste0(outfolder, "bds_", y, "_", i,"_age0", ".csv"))
+
+        Sys.sleep(2)
+    }
+}
+
+# Pull total number of firms by year, NAICS 2-digit sector, and MSA
+for (i in inds) {
+    for (y in years) {
         # Construct the full URL with parameters
         url <- paste0(base_url, "?get=", paste(variables, collapse = ","), "&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&time=", y, "&NAICS=", i, "&key=", api_key)
 
@@ -33,4 +55,3 @@ for (y in years) {
         Sys.sleep(2)
     }
 }
-
